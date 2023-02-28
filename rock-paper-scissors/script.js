66c7d596fed356bda27f7700973615bc4eaeef18
Larissa <larissabastos897@gmail.com>
2023-02-27 23:24:09 -0300

    Finalizado projeto Pedra-papel-tesoura. Próximos passos: melhorar esse projeto

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,12 +1,39 @@
 function getComputerChoice () {
-    let computer = Math.floor(Math.random()*4)
+    let computer = Math.floor(Math.random()*4);
     if (computer === 1) {
-        return ('Rock');
+        return ('rock');
     } else if (computer === 2) {
-        return ('Paper');
+        return ('paper');
     } else {
-        return ('Scissors');
+        return ('scissors');
     }
 }
 
-console.log(getComputerChoice()) 
+let computerSelection = getComputerChoice();
+
+
+let playerSelection = prompt("Choose between: Rock, Paper or Scissors!").toLowerCase();
+
+function round(playerSelection, computerSelection) {
+    
+    if (playerSelection === computerSelection) {
+        return `It's a tie! You and the computer both chose ${playerSelection}.`;
+    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        return `You won! ${playerSelection} beats ${computerSelection}!`;
+    } else if ((playerSelection === 'rock' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'scissors') ||
+    (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    } else {
+        return `Invalid choice! Please choose rock, paper or scissors.`;
+    }
+    
+}
+
+console.log(`Computer chose: ${computerSelection}. You chose ${playerSelection}`);
+
+console.log(round(playerSelection, computerSelection));
+
+// ok a função funciona (não sei se bem), mas continua dando undefined porque não coloquei parâmetros na função. Mas os parâmetros deviam ser auto.
